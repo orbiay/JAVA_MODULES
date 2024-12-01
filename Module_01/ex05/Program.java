a66ff8bfd3d335b89babc8a9dea2449e51fe9dc8
orbiay <orbiay@student.1337.ma>
2024-12-01 16:41:17 +0100

    still loading

--- Module_01/ex05/Program.java
+++ Module_01/ex05/Program.java
@@ -2,27 +2,29 @@ import java.util.UUID;
 
 public class Program {
     public static void main(String[] args) {
-        User user1 = new User( "A", 500);
-        User user2 = new User( "B", 500);
+        // User user1 = new User( "A", 500);
+        // User user2 = new User( "B", 500);
         try{
             // Adding transactions
-            Transaction transactionNum1 = new Transaction(user2,user1,Transaction.Category.DEBITS,-2000);
-            Transaction transactionNum2 = new Transaction(user1,user2,Transaction.Category.CREDITS,2000);
-            user1.setTransaction(transactionNum1);
-            user1.setTransaction(transactionNum2);
-            user2.setTransaction(transactionNum1);
-            user2.setTransaction(transactionNum2);
-
+            // Transaction transactionNum1 = new Transaction(user2,user1,Transaction.Category.DEBITS,-2000);
+            // Transaction transactionNum2 = new Transaction(user1,user2,Transaction.Category.CREDITS,2000);
+            // user1.setTransaction(transactionNum1);
+            // user1.setTransaction(transactionNum2);
+            // user2.setTransaction(transactionNum1);
+            // user2.setTransaction(transactionNum2);
+            Menu menu = new Menu(new TransactionsService(),true);
+            menu.entry();
             // user.getTransactions().addTransaction(t1);
             // user.getTransactions().addTransaction(t2);
 
             // Printing all transactions
-            int size = user1.getSize();
-            Transaction[] transactions = user1.getTransactions();
-            for (int i = 0;i < size ; i++) {
-                System.out.println(transactions[i].getId() + " " + transactions[i].getCategory());
-            }
-        }catch(TransactionNotFoundException e)
+            // int size = user1.getSize();
+            // Transaction[] transactions = user1.getTransactions();
+            // for (int i = 0;i < size ; i++) {
+            //     System.out.println(transactions[i].getId() + " " + transactions[i].getCategory());
+            // }
+
+        }catch(Exception e)
         {
             System.out.println(e.getMessage());
         }
