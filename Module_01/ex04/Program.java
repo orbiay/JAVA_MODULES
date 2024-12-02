2f223ea0042f9aef65268cdf9eacc62d04b6e89c
orbiay <orbiay@student.1337.ma>
2024-12-02 11:00:09 +0100

    ready for correction

--- Module_01/ex04/Program.java
+++ Module_01/ex04/Program.java
@@ -1,28 +1,40 @@
 import java.util.UUID;
 
 public class Program {
+    static public void display(Transaction[] transactions,int size)
+    {
+         
+        for(int i = 0; i < size ;i++ )
+        {
+            Transaction tran = transactions[i];
+            System.out.println("Sender " + tran.getSender().getName() + " recipent is " + tran.getRecipient().getName() + " amount " + tran.getAmount());
+        }
+    }
     public static void main(String[] args) {
-        User user1 = new User( "A", 500);
-        User user2 = new User( "B", 500);
         try{
-            // Adding transactions
-            Transaction transactionNum1 = new Transaction(user2,user1,Transaction.Category.DEBITS,-2000);
-            Transaction transactionNum2 = new Transaction(user1,user2,Transaction.Category.CREDITS,2000);
-            user1.setTransaction(transactionNum1);
-            user1.setTransaction(transactionNum2);
-            user2.setTransaction(transactionNum1);
-            user2.setTransaction(transactionNum2);
-
-            // user.getTransactions().addTransaction(t1);
-            // user.getTransactions().addTransaction(t2);
-
-            // Printing all transactions
-            int size = user1.getSize();
-            Transaction[] transactions = user1.getTransactions();
-            for (int i = 0;i < size ; i++) {
-                System.out.println(transactions[i].getId() + " " + transactions[i].getCategory());
-            }
-        }catch(TransactionNotFoundException e)
+            User userA = new User("A",2000);
+            TransactionsService service = new TransactionsService();
+            service.addAuser(userA);
+            service.addAuser(new User("B",2000));
+            service.addAuser(new User("C",2000));
+            System.out.println("A Balance is "+ service.retrieveUserBalance(1));
+            service.transfer(1,2,-200);
+            service.transfer(2,1,-300);
+            service.transfer(3,1,-300);
+            Transaction[] userATransactions = service.getTransfersForUserById(1);
+            Transaction[] userBTransactions = service.getTransfersForUserById(2);
+            Transaction[] userCTransactions = service.getTransfersForUserById(3);
+            System.out.println("Transaction for User A");
+            display(userATransactions,3);
+            System.out.println("Transaction for User B");
+            display(userBTransactions,2);
+            System.out.println("Transaction for User C");
+            display(userCTransactions,1);
+            service.removeTransactionById(1,userATransactions[0].getId());
+            userATransactions = service.getTransfersForUserById(1);
+            System.out.println("After Remove Transaction for User A");
+            display(userATransactions,2);
+        }catch(Exception e)
         {
             System.out.println(e.getMessage());
         }
