7ec06f596629e8760ae2ee0453ca6c5277282ffa
orbiay <orbiay@student.1337.ma>
2024-12-02 11:00:40 +0100

    ready for correction

--- Module_01/ex04/TransactionsService.java
+++ Module_01/ex04/TransactionsService.java
@@ -1,10 +1,16 @@
+import java.util.UUID;
+
 class TransactionsService {
     private UsersList users;
+    TransactionsService(){
+        users = new UsersArrayList();
+    }
     public void addAuser(User user)
     {
         if (user != null)
             users.addUser(user);
-        throw new UserNotFoundException("There is no user");
+        else
+            throw new UserNotFoundException("There is no user");
     }
 
     public int retrieveUserBalance(int id)
@@ -12,23 +18,23 @@ class TransactionsService {
         return users.getUserById(id).getBalance();
     }
 
-   public void transfer(int senderr , int recipentt,int amount)
-   {
-        User sender = users.getUserById(senderr);
-        User recipent = users.getUserById(recipentt);
-        if (retrieveUserBalance(senderr) < amount)
-            throw new IllegalTransactionException("Sender Balance samller than his transfered amount");
-        //  IDs of both transactions must be equal
-        Transaction transaction = new Transaction(recipent,sender,Category.DEBITS,amount);
-        // so create it first as a DEBITS for the sender    
-        sender.setTransaction(transaction);
-        // change the transaxtion category for the reciepent
-        transaction.setCategory(Category.CREDITS);
-        sender.setTransaction(transaction);
-
-        sender.creditTransfer(amount);
-        recipent.debitTransfer(amount);
-   }
+    public void transfer(int senderr , int recipentt,int amount)
+    {
+         User sender = users.getUserById(senderr);
+         User recipent = users.getUserById(recipentt);
+         if (retrieveUserBalance(senderr) < amount * (-1))
+             throw new IllegalTransactionException("Sender Balance samller than his transfered amount");
+         //  IDs of both transactions must be equal
+         Transaction transaction = new Transaction(recipent,sender,Transaction.Category.DEBITS,amount);
+         // so create it first as a DEBITS for the sender    
+         sender.setTransaction(transaction);
+         // change the transaxtion category for the reciepent
+         transaction.setCategory(Transaction.Category.CREDITS);
+         recipent.setTransaction(transaction);
+ 
+         sender.creditTransfer(amount * (-1));
+         recipent.debitTransfer(amount * (-1));
+    }
 
    public Transaction[] getTransfersForUserById(int id)
    {
@@ -38,13 +44,13 @@ class TransactionsService {
 
    public void  removeTransactionById(int userId, UUID transactionId)
    {
-        User user = users.getUserById(id);
+        User user = users.getUserById(userId);
         user.deleteTransactionById(transactionId);
    }
 
-   public Transaction[] checKValidityOfTransaction()
-   {
+//    public Transaction[] checKValidityOfTransaction()
+//    {
     
-   }
+//    }
 
 }
