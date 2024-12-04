183395c39d1f4b266b1f2126300c176e0b639bfb
orbiay <orbiay@student.1337.ma>
2024-12-04 16:26:55 +0100

    ready for correction

--- Module_02/ex01/Program.java
+++ Module_02/ex01/Program.java
@@ -20,14 +20,14 @@ class Program {
             fileReader.close();
         }catch(Exception e)
         {
-            
+            System.err.println(e.getMessage());
         }
         return stringBuffer;
     }
     public static Map<String,Integer> stringToMap(String fileContent)
     {
         String sp[] = fileContent.split(" ");
-        Map<String,Integer> retMap = new HashMap();
+        Map<String,Integer> retMap = new HashMap<String,Integer>();
         for (int i = 0; i < sp.length ; i++)
         {
             if (retMap.get(sp[i]) == null)
@@ -40,13 +40,12 @@ class Program {
     static public HashSet<String> StringToSet(String content)
     {
         String sp[] = content.split(" ");
-        HashSet<String> newSet = new HashSet();
+        HashSet<String> newSet = new HashSet<String>();
         for (int i = 0;i < sp.length; i++)
         {
             if(!sp[i].equals(""))
             {
                 newSet.add(sp[i]);
-                System.out.println(" =>" + sp[i] +"<=");
             }
         }
         return newSet;
@@ -60,7 +59,6 @@ class Program {
                 vector[i] = 0;
             else
                 vector[i] = map.get(s);
-            System.out.print(vector[i] + " , ");
             i++;
         }
         System.out.println();
@@ -70,7 +68,6 @@ class Program {
         int ret = 0;
         for (int i = 0 ;i < size;i++)
         {
-            System.out.println(vector1[i]);
             ret += vector1[i] * vector2[i];
         }
         return ret;
@@ -87,28 +84,38 @@ class Program {
     }
     static public void main(String args[])
     {
-        System.out.println(args.length);
-        if (args.length > 1)
-        {
-            String file1 = readFile(args[0]);
-            String file2 = readFile(args[1]);
-            // System.out.println("file 1 is " + file1 + "\n file2 is " + file2);
-            String words = file1 + " " + file2;
-            Map<String,Integer>  mapFile1 = stringToMap(file1);
-            Map<String,Integer>  mapFile2 = stringToMap(file2);
-            HashSet<String> setOfUniqueStrings = StringToSet (file1 +" " + file2);
-            int[] file1Vector = new int[setOfUniqueStrings.size()];
-            int[] file2Vector = new int[setOfUniqueStrings.size()];
-            fillIntVecor(file1Vector,setOfUniqueStrings.size(),mapFile1,setOfUniqueStrings);
-            for (int i = 0; i < setOfUniqueStrings.size();i++)
+        try{
+            if (args.length > 1)
             {
-                System.out.println( "-> " + file1Vector[i]);
+                String file1 = readFile(args[0]);
+                String file2 = readFile(args[1]);
+                // System.out.println("file 1 is " + file1 + "\n file2 is " + file2);
+                String words = file1 + " " + file2;
+                Map<String,Integer>  mapFile1 = stringToMap(file1);
+                Map<String,Integer>  mapFile2 = stringToMap(file2);
+                HashSet<String> setOfUniqueStrings = StringToSet (file1 +" " + file2);
+                int[] file1Vector = new int[setOfUniqueStrings.size()];
+                int[] file2Vector = new int[setOfUniqueStrings.size()];
+                fillIntVecor(file1Vector,setOfUniqueStrings.size(),mapFile1,setOfUniqueStrings);
+                fillIntVecor(file2Vector,setOfUniqueStrings.size(),mapFile2,setOfUniqueStrings);
+                int numra = numerator(file1Vector,file2Vector,setOfUniqueStrings.size());
+                double denomina = denomina(file1Vector,file2Vector,setOfUniqueStrings.size());
+                System.out.println("numra = " + numra + " denomina " + denomina);
+                System.out.printf("Similarity = %.2f \n"  ,  numra / denomina);
+                FileWriter file = new FileWriter("dictionary.txt");
+                BufferedWriter dic = new BufferedWriter(file);
+                for (String s : setOfUniqueStrings)
+                {
+                    dic.write(s);
+                    dic.newLine();
+                }
+                dic.close();
             }
-            fillIntVecor(file2Vector,setOfUniqueStrings.size(),mapFile2,setOfUniqueStrings);
-            int numra = numerator(file1Vector,file2Vector,setOfUniqueStrings.size());
-            double denomina = denomina(file1Vector,file2Vector,setOfUniqueStrings.size());
-            System.out.println("numra = " + numra + " denomina " + denomina);
-            System.out.println("Similarity = %.2d"  +  numra / denomina);
+            else
+                System.out.println("Error : Arguments not found");
+        }catch(Exception e)
+        {
+            System.out.println(e.getMessage());
         }
     }
 }
