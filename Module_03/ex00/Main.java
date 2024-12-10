e5184ab579619c91d65840bcd75d988cdad5a9ff
orbiay <orbiay@student.1337.ma>
2024-12-10 11:57:30 +0100

    ready for correction

--- Module_03/ex00/Main.java
+++ Module_03/ex00/Main.java
@@ -1,23 +1,34 @@
 class Main {
     static public void main(String args[])
     {
-        if (args.length < 1 || !args[0].startsWith("--count="))
+        try{
+            if (args.length < 1 || !args[0].startsWith("--count="))
+            {
+                System.out.println("You must enter times.");
+                return ;
+            }
+            int count = Integer.parseInt(args[0].substring("--count=".length()));
+            ThreadNum01 threadEgg = new ThreadNum01(count);
+            ThreadNum02 implementThread = new ThreadNum02(count);
+            Thread threadHenn = new Thread(implementThread);
+            threadEgg.start();
+            threadHenn.start();
+            try {
+                threadEgg.join();
+                threadHenn.join();
+            } catch (InterruptedException e) {
+                System.err.println("Main thread interrupted.");
+            }
+            int i = 0;
+            while(i < count)
+            {
+                System.out.println("Human");
+                i++;
+            }
+            System.out.println("The Main Thread has been Done");
+        }catch(Exception e)
         {
-            System.out.println("You must enter times.");
-            return ;
+            System.err.println(e.getMessage());
         }
-        int count = Integer.parseInt(args[0].substring("--count=".length()));
-        ThreadNum01 threadEgg = new ThreadNum01(count);
-        ThreadNum02 implementThread = new ThreadNum02(count);
-        Thread threadHenn = new Thread(implementThread);
-        threadEgg.start();
-        threadHenn.start();
-        int i = 0;
-        while(i < count)
-        {
-            System.out.println("Human");
-            i++;
-        }
-        System.out.println("The Main Thread has benne Done");
     }
 }
